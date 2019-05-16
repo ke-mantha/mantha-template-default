a901100763056639b52e72f0a8b8314266b0b659
Karen <dev.kaskar2008@gmail.com>
2019-05-16 11:29:17 +0300

    Fix webpack.config/webpack.base - config path

--- webpack.config/webpack.base.ts
+++ webpack.config/webpack.base.ts
@@ -1,7 +1,7 @@
 import { join } from 'path';
 import { webpackBaseConfigFactory, IBaseConfig } from '@mantha/webpack-config';
-import htmlConfigFactory from '../src/.config/build/html';
-import chunkSplitPatterns from '../src/.config/build/chunk';
+import htmlConfigFactory from '../src/config/build/html';
+import chunkSplitPatterns from '../src/config/build/chunk';
 
 export default (mode: IBaseConfig['mode']) => webpackBaseConfigFactory({
   projectPath: join(__dirname, '..'),
